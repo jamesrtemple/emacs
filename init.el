55d5bbc3259e259e3c9f77687e311a6e82b7e1c2
Jim Temple <jamesrtemple@mac.com>
2016-01-15 23:06:38 -0600

    Switched from helm to ido and smex, add rainbow delimeters, and others.

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
 (fringe-mode 1)                                  ;;Enable fringes
 (add-to-list 'custom-theme-load-path             ;;Setup preferred theme
 	     "~/.emacs.d/themes")
-(load-theme 'ample t)
+(load-theme 'granger t)
 (global-visual-line-mode)                        ;;Make line wrap act good
 
 
@@ -112,15 +112,28 @@
 ;;------------------------------------------------------------------------------------------------------
 ;;POWERLINE
 ;;
-(require 'powerline)
-(powerline-default-theme)
+(powerline-center-theme)
 
 
 ;;------------------------------------------------------------------------------------------------------
-;;HELM
+;;IDO
 ;;
-(require 'helm-config)
-(helm-mode 1)
+(ido-mode 1)
+(ido-everywhere 1)
+(ido-ubiquitous-mode 1)
+(setq org-completion-use-ido t)
+(setq magit-completing-read-function 'magit-ido-completing-read)
+(ido-vertical-mode 1)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+
+;;------------------------------------------------------------------------------------------------------
+;;SMEX
+;;
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 
 ;;------------------------------------------------------------------------------------------------------
@@ -128,6 +141,7 @@
 ;;
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
+(setq neo-window-width 40)
 
 
 ;;------------------------------------------------------------------------------------------------------
@@ -154,8 +168,29 @@
 ;;  Loaded before auto complete so that they can work together
 ;;
 (require 'yasnippet)
-(setq helm-yas-space-match-any-greedy t)
-(global-set-key (kbd "M-s M-s") 'helm-yas-complete)
+;; Completing point by some yasnippet key
+(defun yas-ido-expand ()
+  "Lets you select (and expand) a yasnippet key"
+  (interactive)
+    (let ((original-point (point)))
+      (while (and
+              (not (= (point) (point-min) ))
+              (not
+               (string-match "[[:space:]\n]" (char-to-string (char-before)))))
+        (backward-word 1))
+    (let* ((init-word (point))
+           (word (buffer-substring init-word original-point))
+           (list (yas-active-keys)))
+      (goto-char original-point)
+      (let ((key (remove-if-not
+                  (lambda (s) (string-match (concat "^" word) s)) list)))
+        (if (= (length key) 1)
+            (setq key (pop key))
+          (setq key (ido-completing-read "key: " list nil nil word)))
+        (delete-char (- init-word original-point))
+        (insert key)
+        (yas-expand)))))
+(global-set-key (kbd "M-s M-s") 'yas-ido-expand)
 (global-set-key (kbd "M-s s") 'yas-expand)
 (yas-global-mode 1)
 
@@ -188,3 +223,26 @@
 ;; 
 (require 'control-mode)
 (control-mode-default-setup)
+
+
+;;RAINBOW DELIMITERS
+ (add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(require 'smooth-scrolling)
+
+(require 'indent-guide)
+(indent-guide-global-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("743d86e52a227ba0c77e0b6249ec89eb735591a59bdc25ff58dcbfdb0db84d22" "96998f6f11ef9f551b427b8853d947a7857ea5a578c75aa9c4e7c73fe04d10b4" "0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "46fd293ff6e2f6b74a5edf1063c32f2a758ec24a5f63d13b07a20255c074d399" "3cd28471e80be3bd2657ca3f03fbb2884ab669662271794360866ab60b6cb6e6" "998aec317c45ba985ca740770a30cfc5e2ec3a8d5c0d626c011bfcd11f7dabd0" "3cc2385c39257fed66238921602d8104d8fd6266ad88a006d0a4325336f5ee02" "e9776d12e4ccb722a2a732c6e80423331bcb93f02e089ba2a4b02e85de1cf00e" "1a85b8ade3d7cf76897b338ff3b20409cb5a5fbed4e45c6f38c98eee7b025ad4" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "b3775ba758e7d31f3bb849e7c9e48ff60929a792961a2d536edec8f68c671ca5" "7bde52fdac7ac54d00f3d4c559f2f7aa899311655e7eb20ec5491f3b5c533fe8" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
