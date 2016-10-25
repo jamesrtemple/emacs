0b67e81f30ac315cced1d341a603d71de20e64a5
Jim Temple <jamesrtemple@mac.com>
2016-10-24 21:47:45 -0500

    removed old packages

--- elpa/control-mode-20140218.607/control-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "control-mode" "20140218.607" "A \"control\" mode, similar to vim's \"normal\" mode" 'nil :url "https://github.com/stephendavidmarsh/control-mode" :keywords '("convenience" "emulations"))
--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-s;;COMMON SETTINGS / BASIC EMACS CONFIGURATION
+;;COMMON SETTINGS / BASIC EMACS CONFIGURATION
 (setq ansi-color-names-vector                    ;;What does this do? Copied from customize section.
       ["#2e3436" "#a40000" "#4e9a06" "#c4a000"
        "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
@@ -231,3 +231,17 @@ s;;COMMON SETTINGS / BASIC EMACS CONFIGURATION
 
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (win-switch web-mode web-beautify unicode-fonts tern-auto-complete smooth-scrolling smex rainbow-delimiters powerline pandoc-mode org-bullets neotree markdown-mode magit js2-refactor js-comint indent-guide impatient-mode ido-vertical-mode ido-ubiquitous helm-c-yasnippet flycheck expand-region emms emmet-mode browse-kill-ring ace-jump-mode ac-js2))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
