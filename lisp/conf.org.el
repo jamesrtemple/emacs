b71e8c37e47d8adccdf137f4956e9caf9dfcbb20
Jim Temple <jim.temple@optum.com>
2017-03-10 11:09:00 -0600

    Updated org faces.

--- lisp/conf.org.el
+++ lisp/conf.org.el
@@ -6,3 +6,17 @@
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 (add-hook 'org-mode-hook 'flyspell-mode 1)
 
+
+(set-face-attribute 'org-level-1 nil
+                    :height 2.0
+                    :family "Averia-sans"
+                    :foreground "NavajoWhite2"
+                    :weight 'ultra-light
+                    :background nil)
+
+(set-face-attribute 'org-level-2 nil
+                    :height 1.2
+                    :family "Averia-sans"
+                    :foreground "cornsilk3"
+                    :weight 'ultra-light
+                    :background nil)
