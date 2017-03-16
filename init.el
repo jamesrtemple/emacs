d8c30a82e9d27306f3300b0264e32a6d307ed84e
Jim Temple <jim.temple@optum.com>
2017-03-16 15:03:05 -0500

    Moved personal config into conf.master

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -58,66 +65,6 @@
      (360 . "#8bc34a"))))
  '(vc-annotate-very-old-color nil))
 
-
-
-;;COMMON SETTINGS / BASIC EMACS CONFIGURATION
+;;JUMP TO PERSONAL CONFIGURATION
 (add-to-list 'load-path "~/.emacs.d/lisp/")
-(setq auto-save-default nil)                        ;;Disable auto-saved files
-(setq make-backup-files nil)                        ;;Disable auto-backup files
-(setq inhibit-startup-message t)                    ;;No start screen
-(setq ring-bell-function 'ignore)                   ;;Turn off that damn bell!
-(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;;Scrolling
-(setq mouse-wheel-progressive-speed t)
-(setq mouse-wheel-follow-mouse 't)
-(setq scroll-step 1)
-(setq-default indent-tabs-mode nil)
-(setq tab-stop-list (number-sequence 4 120 4))
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'narrow-to-region 'disabled nil)
-(put 'dired-find-alternate-file 'disabled nil)
-(global-visual-line-mode)                           ;;Make line wrap act good
-
-;;Setup options that are invalid in the terminal
-;;or that I only want in graphical version
-(when (display-graphic-p)
-  (scroll-bar-mode 0)                               ;;Set Scroll bars on or off
-  (tool-bar-mode 0)                                 ;;Set toolbar off
-  (fringe-mode 15)                                  ;;Enable fringes
-  (set-frame-size (selected-frame) 90 50)
-)
-
-;;MELPA
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
-
-;;THEME STUFF
-(color-theme-sanityinc-tomorrow-night)
-(setq tabbar-background-color "#263438") ;; the color of the tabbar background
-(window-numbering-mode)
-
-(electric-pair-mode t)
-
-(load "conf.term")
-(load "conf.modeline")
-(load "conf.mac")
-(load "conf.dired")
-(load "conf.winswitch_ace")
-(load "conf.neotree")
-(load "conf.helm")
-(load "conf.progmode")
-(load "conf.text")
-(load "conf.org")
-(load "conf.markdown") ;;and pandoc
-(load "conf.yasnippet")
-(load "conf.company")
-(load "conf.hydra")
-(load "conf.htmlcss")
-(load "conf.javascript")
-(load "conf.typescript")
-(load "conf.projectile")
-(load "conf.keybindings")
-
-(setq winner-mode t)
-
+(load "conf.master")
