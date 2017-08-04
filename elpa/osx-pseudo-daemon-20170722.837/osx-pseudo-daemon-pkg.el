b01bb48c66576e68eeedb8b8f321126620003250
Jim Temple <jim.temple@optum.com>
2017-08-04 14:06:17 -0500

    Package Updates

--- elpa/osx-pseudo-daemon-20170722.837/osx-pseudo-daemon-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "osx-pseudo-daemon" "20170722.837" "Daemon mode that plays nice with OSX." 'nil :url "https://github.com/DarwinAwardWinner/osx-pseudo-daemon" :keywords
-  '("convenience" "osx"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
