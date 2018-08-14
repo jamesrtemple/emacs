c20860cbfdb498b4a0cab85bfc9462a85ad07131
Jim Temple <jim.temple@optum.com>
2018-08-14 08:02:51 -0500

    88 package upgrades

--- elpa/karma-20160220.445/karma-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "karma" "20160220.445" "Karma Test Runner Emacs Integration" '((pkg-info "0.4") (emacs "24")) :url "http://github.com/tonini/karma.el" :keywords '("language" "javascript" "js" "karma" "testing"))
